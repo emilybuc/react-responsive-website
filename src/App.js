7b93fa947a189b686336ff7ada83288a9b9d9cef
emily buckley <emily.buckley@rs-components.com>
2021-07-05 17:20:56 +0100

    Adding in hero banner and menu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/pages/Home'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-    <h1>hola</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar/>
+        <Switch>
+          <Route path='/' exact component = {Home}/>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
